cdef928fa60112bb63cb7ca47a741af26f5bc79c
nikita-kun <6025342+nikita-kun@users.noreply.github.com>
2019-02-28 16:09:59 +0100

    [Refactor] Extract method RootExplorerDB.getIntersections; Move constants;

--- src/RootExplorer.js
+++ src/RootExplorer.js
@@ -66,7 +66,7 @@ class RootExplorer{
 
 			}
 		}
-		
+
 		console.log("Root fetching DONE");
 		updateLogLists();
 		resetExplorer();
@@ -77,56 +77,51 @@ class RootExplorer{
 	function calculateIntersections(depth){
 		var sets = new Array();
 
-		const intersectionQuery2 = "SELECT log.fingerprint, log2.fingerprint, (SELECT count(DISTINCT log_root.root_fingerprint) FROM log_root inner join log_root AS log_root2 ON log_root2.root_fingerprint = log_root.root_fingerprint WHERE log_root.log_fingerprint = log.fingerprint and log_root2.log_fingerprint = log2.fingerprint), log.description AS d, log2.description AS d2 FROM log inner join log AS log2 WHERE log.checked and log2.checked and log.fingerprint >= log2.fingerprint";
-
-		const intersectionQuery3 = "SELECT log.fingerprint, log2.fingerprint, log3.fingerprint, (SELECT count(DISTINCT log_root.root_fingerprint) FROM log_root inner join log_root AS log_root2 ON log_root2.root_fingerprint = log_root.root_fingerprint inner join log_root AS log_root3 ON log_root3.root_fingerprint = log_root.root_fingerprint WHERE log_root.log_fingerprint = log.fingerprint and log_root2.log_fingerprint = log2.fingerprint and log_root3.log_fingerprint = log3.fingerprint), log.description AS d, log2.description AS d2, log3.description AS d3 FROM log inner join log AS log2 inner join log AS log3 WHERE log.checked and log2.checked and log3.checked and log.fingerprint >= log2.fingerprint and log2.fingerprint >= log3.fingerprint group by (log.fingerprint || log2.fingerprint || log3.fingerprint)";
-
+		var intersections = this.db.getIntersections(depth);
 		switch (depth){
 			case 2:
-			var result = db.exec(intersectionQuery2)[0]
 
-			if (typeof result == 'undefined')
+			if (typeof intersections == 'undefined')
 			return sets;
 
-			for (var i=0; i < result.values.length; i++){
-				sets.push({"sets" : (result.values[i][0] == result.values[i][1] ?
-					[result.values[i][0]] :
-					[result.values[i][0], result.values[i][1]]
+			for (var i=0; i < intersections.values.length; i++){
+				sets.push({"sets" : (intersections.values[i][0] == intersections.values[i][1] ?
+					[intersections.values[i][0]] :
+					[intersections.values[i][0], intersections.values[i][1]]
 				),
-				"size": result.values[i][2],
-				"label": (result.values[i][0] == result.values[i][1] ?
-					result.values[i][3] :
-					(result.values[i][3] + "<br>∩<br>" + result.values[i][4])
+				"size": intersections.values[i][2],
+				"label": (intersections.values[i][0] == intersections.values[i][1] ?
+					intersections.values[i][3] :
+					(intersections.values[i][3] + "<br>∩<br>" + intersections.values[i][4])
 				)});
 			}
 
 			break;
 			case 3:
-			var result = db.exec(intersectionQuery3)[0]
 
-			if (typeof result == 'undefined')
+			if (typeof intersections == 'undefined')
 			return sets;
 
 			var label, ids;
 
-			for (var i=0; i < result.values.length; i++){
+			for (var i=0; i < intersections.values.length; i++){
 				switch (true){
-					case (result.values[i][0] == result.values[i][1] && result.values[i][1] == result.values[i][2]):
-					label = result.values[i][4];
-					ids = [result.values[i][0]];
+					case (intersections.values[i][0] == intersections.values[i][1] && intersections.values[i][1] == intersections.values[i][2]):
+					label = intersections.values[i][4];
+					ids = [intersections.values[i][0]];
 					break;
-					case (result.values[i][0] == result.values[i][1] || result.values[i][1] == result.values[i][2]):
-					label = result.values[i][4] + "<br>∩<br>" + result.values[i][6];
-					ids = [result.values[i][0], result.values[i][2]];
+					case (intersections.values[i][0] == intersections.values[i][1] || intersections.values[i][1] == intersections.values[i][2]):
+					label = intersections.values[i][4] + "<br>∩<br>" + intersections.values[i][6];
+					ids = [intersections.values[i][0], intersections.values[i][2]];
 					break;
 
 					default:
-					label = result.values[i][4] + "<br>∩<br>" + result.values[i][5] + "<br>∩<br>" + result.values[i][6];
-					ids = [result.values[i][0], result.values[i][1], result.values[i][2]];
+					label = intersections.values[i][4] + "<br>∩<br>" + intersections.values[i][5] + "<br>∩<br>" + intersections.values[i][6];
+					ids = [intersections.values[i][0], intersections.values[i][1], intersections.values[i][2]];
 				}
 
 				sets[label] = ({"sets" : ids,
-				"size": result.values[i][3],
+				"size": intersections.values[i][3],
 				"label": label});
 			}
 
