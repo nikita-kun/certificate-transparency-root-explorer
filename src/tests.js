eb8b92f42b40f48e936c413c9eb47e4bbd685d18
nikita-kun <6025342+nikita-kun@users.noreply.github.com>
2019-03-01 19:56:43 +0100

    [Test] Test for a blank database; Initialize the database and execute basic methods;

--- src/tests.js
+++ src/tests.js
@@ -1,3 +1,13 @@
+QUnit.module( "Basic tests" );
 QUnit.test( "Browser compatibility", function( assert ) {
   assert.ok( RootExplorer.isCompatibleToBrowser(), "The browser is compatible with the RootExplorer" );
+});
+
+QUnit.module("Database");
+QUnit.test("Blank database", function (assert){
+  db = new RootExplorerDB();
+  assert.ok( db, "Init an SQL.js database with RootExplorer schema");
+  assert.ok( db.listLogs(), "List logs");
+  assert.ok( db.logStats(), "Log stats");
+  assert.notOk( db.getIntersections(2), "Intersections");
 });
